d742e42ad64278f2d7d6de8c0e70fb374d1f9eeb
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-20 15:34:35 -0500

    modified JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-var fs = require("fs");
- 
-module.exports = {
-    cert: fs.readFileSync(__dirname + "/server.cert"),
-    key: fs.readFileSync(__dirname + "/server.key"),
-    passphrase: "12345"
-};
-
 $(function() {
 
     var APIKEY = "hhT2L1pDmHQXlzkxCTAz6S2mfQlHmq8Z"
@@ -13,11 +5,10 @@ $(function() {
         e.preventDefault() // stops the page from reloading 
         var userInput = $('#userInput').val() //gathers search input 
         var URL = $.get('https://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'=5'); //creates URL
-        console.log(URL)
         fetch(URL)
-            .then(response => response.json())
-            .then(content => {
-              console.log(content.data);
-    })
-}) })
+            .then(res => res.json())
+            .then(data => console.log(data))
 
+    })
+})
+ 
