c344fbbb56c4984b9782dc8fa9497dd7396da476
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-20 15:52:28 -0500

    debugging

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ $(function() {
         e.preventDefault() // stops the page from reloading 
         var userInput = $('#userInput').val() //gathers search input 
         var URL = $.get('https://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'=5'); //creates URL
-        fetch(URL)
-            .then(res => res.json())
-            .then(data => console.log(data))
+        console.log(URL)
+        // fetch(URL)
+        //     .then(res => res.json())
+        //     .then(data => console.log(data))
 
     })
 })
