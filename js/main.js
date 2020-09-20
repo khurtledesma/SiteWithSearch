845e0e687e7f6e6c68876a5290f4cb4f40617cd6
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-20 17:16:11 -0500

    udpated JS, now brings images onto the page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 $(function() {
-
     var APIKEY = "hhT2L1pDmHQXlzkxCTAz6S2mfQlHmq8Z"
     $('#userSubmit').click(function (e) { 
         e.preventDefault() // stops the page from reloading 
-        var userInput = $('#userInput').val() //gathers search input 
-        var URL = $.get('https://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'=5'); //creates URL
-        console.log(URL)
-        // fetch(URL)
-        //     .then(res => res.json())
-        //     .then(data => console.log(data))
+        var userInput = $('#userInput').val(); //gathers search input 
+        $.ajax ({
+            url:'http://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'&limit=5',
+            dataType: 'json',
+            type: 'get',
+            cache: false,
+            success: function(items) {
+                $(items.data).each(function (index, value) {
+                    imgSrc = value.images.downsized.url
+                    // console.log(imgSrc)
+                     $('#results').append('<img src='+imgSrc+'>')
 
-        
+            });
+         }
+
+        });
     })
-})
  
+})
+
