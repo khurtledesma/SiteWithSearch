9d7a3a750bcae17bdc9651a1baf6eb4c24b80a86
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-20 17:29:38 -0500

    updated for amount field

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@ $(function() {
     var APIKEY = "hhT2L1pDmHQXlzkxCTAz6S2mfQlHmq8Z"
     $('#userSubmit').click(function (e) { 
         e.preventDefault() // stops the page from reloading 
+        $('#results').empty(); //empties page after submitting, allows you to search multiple times 
         var userInput = $('#userInput').val(); //gathers search input 
+        var userAmount = $('#userAmount').val();
+
         $.ajax ({
-            url:'http://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'&limit=5',
-            dataType: 'json',
-            type: 'get',
+            url:'http://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'&limit='+userAmount+'', //url of the API
+            dataType: 'json', //dataType
+            type: 'get', //getting JSON
             cache: false,
             success: function(items) {
-                $(items.data).each(function (index, value) {
-                    imgSrc = value.images.downsized.url
-                    // console.log(imgSrc)
-                     $('#results').append('<img src='+imgSrc+'>')
+                $(items.data).each(function (index, value) { //cycles through the data received
+                    imgSrc = value.images.downsized.url //gets the img URL
+                     $('#results').append('<img class="col-md-3" alt="img from GIPHY" src='+imgSrc+'>') //adds to the results Div
 
             });
          }
