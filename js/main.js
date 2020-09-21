87596bcbe4874b1eef7b22ecc6de136f5a708170
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-21 13:38:19 -0500

    updated search bar and mobile sizing

--- js/main.js
+++ js/main.js
@@ -4,19 +4,20 @@ $(function() {
         e.preventDefault() // stops the page from reloading 
         $('#results').empty(); //empties page after submitting, allows you to search multiple times 
         var userInput = $('#userInput').val(); //gathers search input 
-        var userAmount = 5  // $('#userAmount').val();
+        var userAmount = 30  // $('#userAmount').val();
         $.ajax ({
             url:'https://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'&limit='+userAmount+'', //url of the API
             dataType: 'json', //dataType
             type: 'get', //getting JSON
             cache: false,
             success: function(items) {
+                console.log(items)
                 $(items.data).each(function (index, value) { //cycles through the data received
                     imgSrc = value.images.downsized.url //gets the img URL
-                     $('#results').append('<img class="col-md-3 col-sm-12" alt="img from GIPHY" src='+imgSrc+'>') //adds to the results Div
+                     $('#results').append('<img class="col-md-4 col-sm-12" alt="img from GIPHY" src='+imgSrc+'>') //adds to the results Div
 
             });
-         }
+         },
 
         });
     })
