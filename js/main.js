642892b80f6a90f7ec7a3805b8fbe8bb2451758e
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-09-22 17:58:20 -0500

    added search results

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(function() {
         $('#results').empty(); //empties page after submitting, allows you to search multiple times 
         var userInput = $('#userInput').val(); //gathers search input 
         var userAmount = 30  // $('#userAmount').val();
+        $('#results').html('<h1>Showing results for: ' + userInput + '</h1>')
         $.ajax ({
             url:'https://api.giphy.com/v1/gifs/search?q='+userInput+'&api_key='+APIKEY+'&limit='+userAmount+'', //url of the API
             dataType: 'json', //dataType
